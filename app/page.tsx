f144440e75105190896546f5a188b9e7eed66b2c
THEMER12 <jackrojo21jk@gmail.com>
2025-09-23 10:51:48 +0200

    add layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session, status } = useSession();
