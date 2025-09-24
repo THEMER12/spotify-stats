fb55b4789eed48a494dd5262b5b7358c0b560e8f
THEMER12 <jackrojo21jk@gmail.com>
2025-09-24 17:26:04 +0200

    home

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,15 +2,50 @@
 
 export default function Header() {
   return (
-    <header
-    className="w-full h-90 bg-cover bg-center"
-    style={{ backgroundImage: "url('/header.jpg')" }}
-    >
-    <div className="flex items-center justify-center h-full">
-        <h1 className="text-white text-4xl font-bold">Hello</h1>
-    </div>
+    <header>
+      <div
+        className="
+          w-full 
+          h-[22rem] sm:h-[28rem] md:h-[35rem] 
+          flex items-end 
+          bg-cover bg-center
+        "
+        style={{
+          backgroundImage: "url('/header.jpg')",
+          backgroundAttachment: "fixed",
+          backgroundSize: "cover",
+          backgroundPosition: "bottom",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <h1
+          className="
+            ml-6 sm:ml-10 
+            mb-4 sm:mb-6 
+            uppercase 
+            text-[2rem] sm:text-[3rem] md:text-[4.8rem] 
+            text-[var(--orange-web)]
+          "
+        >
+          Tracklytics™ <br />
+          <i>STATS co.</i>
+        </h1>
+      </div>
+
+      <div className="flex items-center justify-start">
+        <h1
+          className="
+            ml-6 sm:ml-10 
+            mb-4 sm:mb-6 
+            uppercase 
+            text-[2rem] sm:text-[3rem] md:text-[4.8rem] 
+            text-[var(--platinum)]
+          "
+        >
+          GEN. & VIS. in: <br />
+          BCN-ESP № 24-MMXXV
+        </h1>
+      </div>
     </header>
-
-
   );
 }
