59ef0bc1cc941b7d692e91115b21cd0520c6bb7f
THEMER12 <jackrojo21jk@gmail.com>
2025-09-27 09:55:48 +0200

    update header from home

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -32,19 +32,27 @@ export default function Header() {
         </h1>
       </div>
 
-      <div className="flex items-center justify-start">
-        <h1
-          className="
-            ml-6 sm:ml-10 
-            mb-4 sm:mb-6 
-            uppercase 
-            text-[2rem] sm:text-[3rem] md:text-[4.8rem] 
-            text-[var(--platinum)]
+      <div className=" grid grid-cols-2 items-center px-6 sm:px-10 gap-6">
+        <h1 className="
+          uppercase 
+          text-[1.5rem] sm:text-[2rem] md:text-[3rem] lg:text-[4.8rem] 
+          text-[var(--platinum)]
           "
         >
           GEN. & VIS. in: <br />
           BCN-ESP № 24-MMXXV
         </h1>
+
+        <div className="flex justify-center md:justify-end">
+          <img
+            src="/logo-black.png"
+            alt="logo"
+            className="
+                h-[7rem] sm:h-[6rem] md:h-[10rem] md:mr-[15rem] lg:h-[15rem] xl:h-[20rem] 
+                object-contain
+              "
+          />
+        </div>
       </div>
     </header>
   );
